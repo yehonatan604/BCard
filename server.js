65505b6613fd8ed213cd5c375b880328ba1845eb
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2023-11-01 01:10:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ const {
   generateInitialUsers,
 } = require("./initialData/initialDataService");
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    // allow preflight
-    if (req.method === 'OPTIONS') {
-        res.send(200);
-    } else {
-        next();
-    }
+app.use(cors());
 });app.use(logger);
 app.use(express.json());
 app.use(express.static("./public"));
