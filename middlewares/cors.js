f8885d4ac9a496f2c1c2acb41c60b62a8e73e959
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2023-11-01 11:13:21 +0200

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,12 +3,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(
-  cors({
-    origin: ["192.168.1.178:3000", "http://localhost:3000", "*"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: true,
-    code: 200,
-  })
+  cors()
 );
 
 module.exports = app;
