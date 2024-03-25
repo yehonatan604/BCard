93c5ec69b417044ea78106832f708ee72c9fa89b
spelltox <39013415+spelltox@users.noreply.github.com>
2024-03-25 14:18:05 +0200

    Update router.js - limiter fix

--- router/router.js
+++ router/router.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const cardsRestController = require("../cards/routes/cardsRestController");
 const usersRestController = require("../users/routes/usersRestController");
 const openaiRoutes = require('../openai/openaiRoutes');
+const { dailyLimit, hourlyLimit } = require("../middlewares/rateLimiters");
+
 const { handleError } = require("../utils/handleErrors");
 
 router.use("/cards", cardsRestController);
 router.use("/users", usersRestController);
+
+openaiRoutes.use(hourlyLimit);
+openaiRoutes.use(dailyLimit);
 router.use('/elran/openai', openaiRoutes);
 
 router.use((req, res) => {
