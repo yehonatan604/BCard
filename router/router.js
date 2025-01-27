a9cb3103ae8b293716b7defdff02ddba46d7468c
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-01-27 10:03:57 +0200

    Update router.js

--- router/router.js
+++ router/router.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const cardsRestController = require("../cards/routes/cardsRestController");
 const usersRestController = require("../users/routes/usersRestController");
-const openaiRoutes = require('../openai/openaiRoutes');
-const { dailyLimit, hourlyLimit } = require("../middlewares/rateLimiters");
+//const openaiRoutes = require('../openai/openaiRoutes');
+//const { dailyLimit, hourlyLimit } = require("../middlewares/rateLimiters");
 
 const { handleError } = require("../utils/handleErrors");
 
 router.use("/cards", cardsRestController);
 router.use("/users", usersRestController);
 
-openaiRoutes.use(hourlyLimit);
-openaiRoutes.use(dailyLimit);
-router.use('/elran/openai', openaiRoutes);
+// openaiRoutes.use(hourlyLimit);
+// openaiRoutes.use(dailyLimit);
+// router.use('/elran/openai', openaiRoutes);
 
 router.use((req, res) => {
   handleError(res, 404, "Page not found!");
